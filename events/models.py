f068873351860022551671a819b5705892586ff1
Mateokruljac <mateokruljac23@gmail.com>
2022-10-02 15:07:22 +0200

    First tests

--- events/models.py
+++ events/models.py
@@ -56,6 +56,9 @@ class Event(models.Model):
         today = datetime.date.today()
         if self.event_date.date() < today:
              time = "Past"
+        elif self.event_date.date() == today:
+            time = "Today"
+            
         else:
             time = "Future"
             
